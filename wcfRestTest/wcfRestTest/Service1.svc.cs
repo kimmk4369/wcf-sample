aea459234937e2a9f176b9795816f832bd79896a
kimmk4369 <killer5634@naver.com>
2021-06-10 16:25:56 +0900

    create

---
+++ wcfRestTest/wcfRestTest/Service1.svc.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace wcfRestTest
+{
+    // 참고: "리팩터링" 메뉴에서 "이름 바꾸기" 명령을 사용하여 코드, svc 및 config 파일에서 클래스 이름 "Service1"을 변경할 수 있습니다.
+    // 참고: 이 서비스를 테스트하기 위해 WCF 테스트 클라이언트를 시작하려면 솔루션 탐색기에서Service1.svc나 Service1.svc.cs를 선택하고 디버깅을 시작하십시오.
+    public class Service1 : IService1
+    {
+        public Product GetData(string product)
+        {
+            Product goodsInfo = new Product();
+            goodsInfo.product = product;
+            goodsInfo.price = 5000;
+
+            return goodsInfo;
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
